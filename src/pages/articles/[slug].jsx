981f17b1fb820d118b2629698be9cd0bb71e4e41
Jovert Palonpon <jovertical@gmail.com>
2020-12-21 02:03:23 +0800

    wip

--- src/pages/articles/[slug].jsx
+++ src/pages/articles/[slug].jsx
@@ -1,5 +1,6 @@
 import ErrorPage from 'next/error'
 import { useRouter } from 'next/router'
+import dayjs from 'dayjs'
 import * as query from '@/queries/article'
 import { markdownToHtml } from '@/helpers'
 import Layout from '@/components/Layout'
@@ -17,21 +18,27 @@ export default function Article({ article }) {
       {router.isFallback ? (
         <p>Loading...</p>
       ) : (
-        <div className="mt-16">
+        <div className="mt-16 ">
           <article className="pb-12">
             <SEO
               title={article.title + ' - Jovert Palonpon'}
               description={article.excerpt}
             />
 
-            <header className="max-w-xl lg:max-w-3xl mx-auto px-6 sm:px-10 lg:px-0">
+            <header className="max-w-2xl lg:max-w-4xl mx-auto">
+              <div className="text-sm text-tertiary dark:text-tertiary-dark tracking-normal">
+                <time dateTime={article._publishedAt}>
+                  {dayjs(article._publishedAt).format('MMMM DD, YYYY')}
+                </time>
+              </div>
+
               <h1 className="text-primary dark:text-primary-dark text-2xl leading-snug sm:text-3xl md:text-4xl lg:text-5xl lg:leading-tight font-bold">
                 {article.title}
               </h1>
             </header>
 
             <div
-              className="mt-6 sm:mt-8 md:mt-10 lg:mt-12 max-w-2xl lg:max-w-4xl mx-auto prose dark:prose-dark lg:prose-lg xl:prose-xl"
+              className="max-w-2xl lg:max-w-4xl mx-auto mt-6 sm:mt-8 md:mt-10 lg:mt-12 prose dark:prose-dark lg:prose-lg xl:prose-xl"
               dangerouslySetInnerHTML={{ __html: article.body }}
             />
           </article>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,34 +1,15 @@
 import Layout from '@/components/Layout'
 import SEO from '@/components/SEO'
-import { markdownToHtml } from '@/helpers'
-import * as query from '@/queries/profile'
 
-export default function Home({ profile }) {
+export default function Home() {
   return (
     <Layout>
-      <article className="prose dark:prose-dark lg:prose-lg">
-        <header>
-          <SEO
-            title="Jovert Palonpon"
-            description="Jovert Palonpon is a Full Stack web developer"
-          />
-        </header>
-
-        <div dangerouslySetInnerHTML={{ __html: profile.about }} />
-      </article>
+      <header>
+        <SEO
+          title="Jovert Palonpon"
+          description="Jovert Palonpon is a Full Stack web developer"
+        />
+      </header>
     </Layout>
   )
-}
-
-export async function getStaticProps() {
-  const profile = await query.get()
-
-  return {
-    props: {
-      profile: {
-        ...profile,
-        about: await markdownToHtml(profile.about),
-      },
-    },
-  }
 }
