324ac59f276a0807fd809e34e1fd89248df31199
Jovert Palonpon <jovertical@gmail.com>
2020-12-24 16:53:34 +0800

    fixed: fs/promises incompatibility

--- src/pages/api/mock.js
+++ src/pages/api/mock.js
@@ -1,15 +1,27 @@
-import fs from 'fs/promises'
+import fs from 'fs'
 import { sleep } from '@/helpers'
 
 export default async (req, res) => {
   if (req.method === 'POST') {
     let data = JSON.stringify(req.body)
 
-    await fs.writeFile('src/mocks/data.json', data)
+    await writeAsync('src/mocks/data.json', data)
     await sleep()
 
     return res.end(data)
   }
 
   return res.end()
+}
+
+async function writeAsync(path, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, data, (err) => {
+      if (err) {
+        return reject(err)
+      }
+
+      resolve()
+    })
+  })
 }
