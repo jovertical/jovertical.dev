9b9fb6b873ecbcd5b8c2216a2de86e787e059dae
Jovert Palonpon <jovertical@gmail.com>
2020-12-25 23:53:47 +0800

    refactor: Extract icons

---
+++ src/components/Icon.jsx
@@ -0,0 +1,27 @@
+import cx from 'classnames'
+
+export default function Icon({
+  size = 'regular',
+  variant = 'filled',
+  className,
+  children,
+}) {
+  return (
+    <svg
+      className={cx(
+        {
+          'w-4 h-4': size === 'small',
+          'w-6 h-6': size === 'regular',
+          'w-8 h-8': size === 'large',
+        },
+        className
+      )}
+      fill={variant === 'filled' ? 'currentColor' : 'none'}
+      stroke={variant === 'outlined' ? 'currentColor' : 'none'}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {children}
+    </svg>
+  )
+}
--- src/components/ThemeFlipper.jsx
+++ src/components/ThemeFlipper.jsx
@@ -1,4 +1,7 @@
 import * as React from 'react'
+import Icon from '@/components/Icon'
+import MoonIcon from '@/components/icons/Moon'
+import SunIcon from '@/components/icons/Sun'
 import { ThemeContext } from '@/contexts/themeContext'
 
 export default function ThemeFlipper() {
@@ -10,37 +13,9 @@ export default function ThemeFlipper() {
       title={`Switch to ${theme.dark ? 'Light mode' : 'Dark mode'}`}
       onClick={theme.flip}
     >
-      {theme.dark ? (
-        <svg
-          className="w-7 h-7"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-          />
-        </svg>
-      ) : (
-        <svg
-          className="w-7 h-7"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-          />
-        </svg>
-      )}
+      <Icon variant="outlined" size="large">
+        {theme.dark ? <MoonIcon /> : <SunIcon />}
+      </Icon>
     </button>
   )
 }
