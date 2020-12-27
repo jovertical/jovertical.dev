3ed353d4c82770051da8949f4cdacff50dac3962
Jovert Palonpon <jovertical@gmail.com>
2020-12-27 19:11:25 +0800

    remove nprogress

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,4 @@
-import dynamic from 'next/dynamic'
 import classnames from 'classnames'
-import 'nprogress/nprogress.css'
 import * as helpers from '@/helpers'
 import expose from '@/helpers/expose'
 import '@/styles/app.css'
@@ -14,13 +12,6 @@ if (process.env.TEST_ENV === 'integration') {
   require('@/mocks')
 }
 
-const Progress = dynamic(() => import('@/components/Progress'), { ssr: false })
-
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Progress />
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
