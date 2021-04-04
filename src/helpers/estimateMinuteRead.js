dd44d30f51d52f2088765874920843dec10265d8
Jovert Palonpon <jovertical@gmail.com>
2021-04-04 15:22:33 +0800

    Update minute read estimation

--- src/helpers/estimateMinuteRead.js
+++ src/helpers/estimateMinuteRead.js
@@ -6,5 +6,5 @@ export default async function estimateMinuteRead(content) {
     let result = await unified().use(markdown).use(stringify).process(content);
     let characterCount = result.toString().length;
 
-    return Math.round(characterCount / 400) || 1;
+    return Math.round(characterCount / 650) || 1;
 }
